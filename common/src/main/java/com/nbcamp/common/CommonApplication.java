86dad7ad4e58c784eca59af9f678511a2fbd2bee
huhsiyoung <july433@naver.com>
2025-11-05 18:01:09 +0900

    fix(root) : 그래들 빌드 안되는 오류 수정

--- common/src/main/java/com/nbcamp/common/CommonApplication.java
+++ common/src/main/java/com/nbcamp/common/CommonApplication.java
@@ -2,10 +2,8 @@ package com.nbcamp.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class CommonApplication {
 
     public static void main(String[] args) {
