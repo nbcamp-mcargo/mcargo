4134dd650efbabe1d39e8b7e0c424192d2987c82
huhsiyoung <july433@naver.com>
2025-11-10 15:51:54 +0900

    remove: delivery jpa 제거

--- delivery-service/src/main/java/com/mcargo/deliveryservice/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/com/mcargo/deliveryservice/DeliveryServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,7 +12,6 @@ public class DeliveryServiceApplication {
 
     @Configuration
     @Profile("!test")
-    @EnableJpaAuditing
     static class JpaAuditingConfig {
     }
 
