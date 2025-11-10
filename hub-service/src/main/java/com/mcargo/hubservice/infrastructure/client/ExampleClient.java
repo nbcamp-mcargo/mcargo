6131cd59b6695df8b47855f3d6b47a2805713017
gyoseok <dhkk65@gmail.com>
2025-11-10 15:02:31 +0900

    ref: 클라이언트 작성 예시 패키지 경로 변경

--- hub-service/src/main/java/com/mcargo/hubservice/infrastructure/client/ExampleClient.java
+++ hub-service/src/main/java/com/mcargo/hubservice/infrastructure/client/ExampleClient.java
@@ -1,4 +1,4 @@
-package com.mcargo.hubservice.infrastructure.repository.client;
+package com.mcargo.hubservice.infrastructure.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
