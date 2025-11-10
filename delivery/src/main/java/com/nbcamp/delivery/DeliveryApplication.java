4fc5aac397afce0bb50b377825705c4a0ba03ee3
huhsiyoung <july433@naver.com>
2025-11-10 12:48:50 +0900

    fix: DeliveryApplication 수정

--- delivery/src/main/java/com/nbcamp/delivery/DeliveryApplication.java
+++ delivery/src/main/java/com/nbcamp/delivery/DeliveryApplication.java
@@ -3,15 +3,21 @@ package com.nbcamp.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableJpaAuditing
 public class DeliveryApplication {
+
+    @Configuration
+    @Profile("!test")
+    @EnableJpaAuditing
+    static class JpaAuditingConfig {
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeliveryApplication.class, args);
     }
-
 }
