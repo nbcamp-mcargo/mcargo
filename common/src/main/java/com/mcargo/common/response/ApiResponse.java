da89e830e30e2085dbcf4c6b9a138fc01a88272f
gyoseok <dhkk65@gmail.com>
2025-11-10 15:04:25 +0900

    ref: 공통 응답 @Getter 추가

--- common/src/main/java/com/mcargo/common/response/ApiResponse.java
+++ common/src/main/java/com/mcargo/common/response/ApiResponse.java
@@ -3,8 +3,10 @@ package com.mcargo.common.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 @Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
