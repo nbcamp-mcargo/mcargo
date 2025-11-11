14a79c4aa619e72ba9086a5c860aa93b6f396441
gyoseok <dhkk65@gmail.com>
2025-11-11 15:11:44 +0900

    ref: 내부 클라이언트 호출도 서비스 로직에서 알지 못하게 변경

--- hub-service/src/main/java/com/mcargo/hubservice/application/service/HubService.java
+++ hub-service/src/main/java/com/mcargo/hubservice/application/service/HubService.java
@@ -1,5 +1,7 @@
 package com.mcargo.hubservice.application.service;
 
+import com.mcargo.hubservice.application.port.CompanyPort;
+import com.mcargo.hubservice.application.port.UserPort;
 import com.mcargo.hubservice.domain.entity.HubProductStatus;
 import com.mcargo.hubservice.domain.exception.HubException;
 import com.mcargo.hubservice.domain.response.HubResponseCode;
@@ -7,8 +9,6 @@ import com.mcargo.common.util.PageingUtils;
 import com.mcargo.hubservice.domain.entity.Hub;
 import com.mcargo.hubservice.domain.entity.HubProduct;
 import com.mcargo.hubservice.domain.repository.HubRepository;
-import com.mcargo.hubservice.infrastructure.client.CompanyClient;
-import com.mcargo.hubservice.infrastructure.client.UserClient;
 import com.mcargo.hubservice.presentation.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -26,8 +26,8 @@ import java.util.stream.Collectors;
 public class HubService {
 
     private final HubRepository hubRepository;
-    private final CompanyClient companyClient;
-    private final UserClient userClient;
+    private final CompanyPort companyPort;
+    private final UserPort userPort;
 
     //ㅡㅡ 허브 관련 ㅡㅡ
     // 허브 생성
