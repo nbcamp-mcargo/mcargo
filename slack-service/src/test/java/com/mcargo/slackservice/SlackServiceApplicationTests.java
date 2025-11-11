d00c93a5209b3e64f875dd557e9de8387fa59b3e
gyoseok <dhkk65@gmail.com>
2025-11-11 19:19:13 +0900

    feat: 초기 셋팅 application.java
    @EnableJpaAuditing
    @EnableFeignClients

---
+++ slack-service/src/test/java/com/mcargo/slackservice/SlackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcargo.slackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
