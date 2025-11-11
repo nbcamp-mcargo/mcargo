9291e8e39fc8b558359a13ec4438383cc9b2db7a
gyoseok <dhkk65@gmail.com>
2025-11-11 09:55:28 +0900

    ref: 허브상품 추가 요청dto 수정
    UUID타입에 NotBlank -> NotNull

--- hub-service/src/main/java/com/mcargo/hubservice/presentation/dto/addHubProductRequest.java
+++ hub-service/src/main/java/com/mcargo/hubservice/presentation/dto/addHubProductRequest.java
@@ -1,19 +1,18 @@
 package com.mcargo.hubservice.presentation.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record addHubProductRequest(
         // 업체 관리자가 업체상품을 허브에 적재? 하겠다는 요청
-        @NotBlank(message = "적재할 허브의 id를 입력하세요.")
+        @NotNull(message = "적재할 허브의 id를 입력하세요.")
         UUID hubId,
 
-        @NotBlank(message = "등록할 업체상품의 id를 입력하세요.")
+        @NotNull(message = "등록할 업체상품의 id를 입력하세요.")
         UUID productId, // 업체상품의 uuid
 
-        @NotBlank(message = "등록할 상품의 재고를 입력하세요.")
+        @NotNull(message = "등록할 상품의 재고를 입력하세요.")
         Integer stock
 ) {
 }
