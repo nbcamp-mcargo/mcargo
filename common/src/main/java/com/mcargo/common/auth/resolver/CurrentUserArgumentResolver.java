e85586ad96f3f59b3f39ce2c36d03490fba33bbd
gyoseok <dhkk65@gmail.com>
2025-11-12 11:23:40 +0900

    ref: 머지 후 에러파일 임시 주석

--- common/src/main/java/com/mcargo/common/auth/resolver/CurrentUserArgumentResolver.java
+++ common/src/main/java/com/mcargo/common/auth/resolver/CurrentUserArgumentResolver.java
@@ -1,48 +1,48 @@
-package com.mcargo.common.auth.resolver;
-
-import com.mcargo.common.auth.annotation.CurrentUser;
-import com.mcargo.common.exception.UserException;
-import com.mcargo.common.response.UserResponseCode;
-import jakarta.annotation.Nullable;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
-
-@Component
-public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
-
-    public static final String HEADER_USER_ID = "X-User-Id";
-
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(CurrentUser.class)
-            && Long.class.isAssignableFrom(parameter.getParameterType());
-    }
-
-    @Override
-    public Object resolveArgument(
-        MethodParameter parameter,
-        @Nullable ModelAndViewContainer mavContainer,
-        NativeWebRequest webRequest,
-        @Nullable WebDataBinderFactory binderFactory) throws Exception {
-        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-        if (request == null) {
-            throw new UserException(UserResponseCode.NO_REQUEST_AVAILABLE);
-        }
-        String userIdHeader = request.getHeader(HEADER_USER_ID);
-        if (userIdHeader == null || userIdHeader.isBlank()) {
-            // 선택: 인증이 필수라면 401(Unauthorized)
-            throw new UserException(UserResponseCode.MISSING_USER_ID);
-        }
-        try {
-            return Long.parseLong(userIdHeader);
-        } catch (NumberFormatException e) {
-            throw new UserException(UserResponseCode.INVALID_USER_ID);
-        }
-    }
-
-}
+//package com.mcargo.common.auth.resolver;
+//
+//import com.mcargo.common.auth.annotation.CurrentUser;
+//import com.mcargo.common.exception.UserException;
+//import com.mcargo.common.response.UserResponseCode;
+//import jakarta.annotation.Nullable;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.core.MethodParameter;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.bind.support.WebDataBinderFactory;
+//import org.springframework.web.context.request.NativeWebRequest;
+//import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+//import org.springframework.web.method.support.ModelAndViewContainer;
+//
+//@Component
+//public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+//
+//    public static final String HEADER_USER_ID = "X-User-Id";
+//
+//    @Override
+//    public boolean supportsParameter(MethodParameter parameter) {
+//        return parameter.hasParameterAnnotation(CurrentUser.class)
+//            && Long.class.isAssignableFrom(parameter.getParameterType());
+//    }
+//
+//    @Override
+//    public Object resolveArgument(
+//        MethodParameter parameter,
+//        @Nullable ModelAndViewContainer mavContainer,
+//        NativeWebRequest webRequest,
+//        @Nullable WebDataBinderFactory binderFactory) throws Exception {
+//        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+//        if (request == null) {
+//            throw new UserException(UserResponseCode.NO_REQUEST_AVAILABLE);
+//        }
+//        String userIdHeader = request.getHeader(HEADER_USER_ID);
+//        if (userIdHeader == null || userIdHeader.isBlank()) {
+//            // 선택: 인증이 필수라면 401(Unauthorized)
+//            throw new UserException(UserResponseCode.MISSING_USER_ID);
+//        }
+//        try {
+//            return Long.parseLong(userIdHeader);
+//        } catch (NumberFormatException e) {
+//            throw new UserException(UserResponseCode.INVALID_USER_ID);
+//        }
+//    }
+//
+//}
