e3c24bcf0437fdd25cb85111a8a5e0a37968fbbc
matino0216 <woqhr33@naver.com>
2025-11-12 16:52:10 +0900

    Resolver 전체 주석 처리

--- common/src/main/java/com/mcargo/common/auth/config/WebMvcConfig.java
+++ common/src/main/java/com/mcargo/common/auth/config/WebMvcConfig.java
@@ -1,18 +1,18 @@
-package com.mcargo.common.auth.config;
-
-import com.mcargo.common.auth.resolver.CurrentUserArgumentResolver;
-import java.util.List;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addArgumentResolvers(
-        List<HandlerMethodArgumentResolver> resolvers
-    ) {
-        resolvers.add(new CurrentUserArgumentResolver());
-    }
-}
+//package com.mcargo.common.auth.config;
+//
+//import com.mcargo.common.auth.resolver.CurrentUserArgumentResolver;
+//import java.util.List;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addArgumentResolvers(
+//        List<HandlerMethodArgumentResolver> resolvers
+//    ) {
+//        resolvers.add(new CurrentUserArgumentResolver());
+//    }
+//}
