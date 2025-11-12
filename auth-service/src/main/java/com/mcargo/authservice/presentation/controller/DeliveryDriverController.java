82ae3ee8b2086366c8d001e0d096d888dd85697b
gyoseok <dhkk65@gmail.com>
2025-11-12 16:38:21 +0900

    ref: 배송 담당자 배정 응답 시 ApiResponse 삭제

--- auth-service/src/main/java/com/mcargo/authservice/presentation/controller/DeliveryDriverController.java
+++ auth-service/src/main/java/com/mcargo/authservice/presentation/controller/DeliveryDriverController.java
@@ -21,10 +21,8 @@ public class DeliveryDriverController {
      * @return
      */
     @GetMapping("/next-driver")
-    public ApiResponse<UUID> getNextDriver(@RequestParam UUID hubId) {
-        UUID driver = deliveryDriverService.getNextDriver(hubId);
-
-        return ApiResponse.of(DriverResponseCode.USER_GET_NEXT_DRIVER, driver);
+    public UUID getNextDriver(@RequestParam(required = false) UUID hubId) {
+        return deliveryDriverService.getNextDriver(hubId);
     }
 
     @PostMapping("/new-driver-seq")
